d6095e8acc86b66213d6006e31d85a0e86a76dfc
goodydev44 <goodyewoma44@gmail.com>
2024-08-18 19:26:27 +0100

    countdown component config

--- src/components/CountDown.jsx
+++ src/components/CountDown.jsx
@@ -1,34 +1,39 @@
 import React, { useState, useEffect } from "react";
 
 const CountDownTimer = () => {
-    const [counter, setCounter] = useState(60);
+  const [seconds, setSeconds] = useState(10);
 
-    useEffect(() => {
-        if (counter > 0) {
-            const timerId = setInterval(() => {
-                setCounter((prevCounter) => prevCounter - 1);
-            }, 1000);
+  const [min, setMin] = useState(2);
 
-            return () => clearInterval(timerId); // Cleanup the interval on component unmount
-        }
-    }, [counter]);
+  useEffect(() => {
+    if (seconds > 0) {
+      const timerId = setInterval(() => {
+        setSeconds((prevSec) => prevSec - 1);
+      }, [1000]);
 
-    return (
-        <div className="grid grid-flow-col gap-5 text-center auto-cols-max">
-            <div className="flex flex-col">
-                <span className="countdown font-mono text-5xl">
-                    <span style={{ "--value": 24 }}></span>
-                </span>
-                min
-            </div>
-            <div className="flex flex-col">
-                <span className="countdown font-mono text-5xl">
-                    <span style={{ "--value": counter }}></span>
-                </span>
-                sec
-            </div>
-        </div>
-    );
+      return () => clearInterval(timerId); // Cleanup the interval on component unmount
+    } else if (min > 0) {
+      setMin((prevMin) => prevMin - 1);
+      setSeconds(9);
+    }
+  }, [seconds, min]);
+
+  return (
+    <div className="grid grid-flow-col gap-5 text-center auto-cols-max">
+      <div className="flex flex-col">
+        <span className="countdown font-mono text-5xl">
+          <span style={{ "--value": min }}></span>
+        </span>
+        min
+      </div>
+      <div className="flex flex-col">
+        <span className="countdown font-mono text-5xl">
+          <span style={{ "--value": seconds }}></span>
+        </span>
+        sec
+      </div>
+    </div>
+  );
 };
 
 export default CountDownTimer;
